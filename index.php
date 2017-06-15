2725d32a6f0f0ce0593124b71a657ed1a568aa77
Pinkaro <schaller.alexander@hotmail.com>
2017-06-15 19:47:05 +0200

    alex

--- index.php
+++ index.php
@@ -7,20 +7,21 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>DIBWARS</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="./res/css/style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
         <div class="container">
-            <div class="nav_left">
-                
+            <div class="nav_left col-md-4">
+                <p>NAV</p>
             </div>
-            <div class="container_main">
+            <div class="container_main col-md-8">
                 <div class="infoboard">
-                    
+                    <p>INFO</p>
                 </div>
                 <div class="databoard">
-                    
+                    <p>DATA</p>
                 </div>
             </div>
         </div>       
