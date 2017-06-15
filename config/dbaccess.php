7e49363ab4df7479062431ac7dcf104eb1f0e02f
NexX <NexX@DESKTOP-L8MI8K7>
2017-06-15 15:33:55 +0200

    address model

---
+++ config/dbaccess.php
@@ -0,0 +1,28 @@
+<?php
+
+class DB {
+
+    private $host = "wi-projectdb.technikum-wien.at";
+    private $user = "s17-bvz2-fst-23";
+    private $password = "DbPass4v023";
+    private $dbname = "s17-bvz2-fst-20";
+    private $connection = null;
+
+    function connect2DB() {
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname);
+        return $this->connection;
+    }
+
+    function getAddress() {
+        $this->connect2DB();
+        $query = "SELECT * FROM ADDRESS";
+        $ergebnis = $this->connection->query($query);
+        if ($ergebnis) {
+            $zeile = $ergebnis->fetch_object();
+            echo '<p>'.$zeile->address.'</p>';
+        }
+        $this->connection->close();
+        return true;
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>The sky is blue!</title>
+        <link rel="stylesheet" type="text/css" href="res/css/style.css">
+    </head>
+    <body class="disco">
+        <h1>NexX war hier!</h1>
+        <h2>Hello!</h2>
+        <div class="liiinks">
+            <img src="picture/alex.png" class="khaled_dreht_sich">            
+        </div>
+        <div class="mittttte">
+            <img src="picture/markus.png" class="markus_vergroessert_sich">            
+        </div>
+        <div class="liiinks">
+            <img src="picture/khaled.png" class="khaled_dreht_sich_anders">            
+        </div>
+        
+        
+        <div>
+            <p>what a DIGITAL BEAUTY</p>
+        </div>
+    </body>
+</html>
