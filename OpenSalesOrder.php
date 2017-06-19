291b6b00be308320c64c8b070ef9f09348b1dc10
wi15b189 <wi15b189@technikum-wien.at>
2017-06-19 11:35:10 +0200

    nav

--- OpenSalesOrder.php
+++ OpenSalesOrder.php
@@ -1,11 +1,26 @@
 <?php include 'inc/header.php'; ?>
+<!--filter hohlen-->
+<?php
+if (empty($_GET['filter'])) {
+    $load = 1;
+} else {
+    $load = $_GET['filter'];
+}
+?>
+<!--Navigation-->
+<h1 class="page-header">Sales overview</h1>
+<nav class="navbar  navbar-inverse">
+    <div class="container">
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul id="menu" class="nav navbar-nav">
+                <li><a href = "OpenSalesOrder.php?filter=1">Open</a></li>
+                <li><a href = "OpenSalesOrder.php?filter=2">In process</a></li>
+                <li><a href = "OpenSalesOrder.php?filter=3">Closed</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>openpurchaseorder
 
-<?php  include 'inc/footer.php';?>
+<?php include 'inc/footer.php'; ?>
