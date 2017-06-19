7c5a765bf103b731a6f72d573939f38282969708
Pinkaro <schaller.alexander@hotmail.com>
2017-06-19 10:44:22 +0200

    frontend edited as discussed

--- inc/navigation_bar.php
+++ inc/navigation_bar.php
@@ -8,45 +8,40 @@
     - change active for each link
 -->
 
-<?php include 'config/Navigation.php';?>
+<?php include 'config/Navigation.php'; ?>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">           
-            
-            <a class="navbar-brand" href="index.php">  Home </a>
+<div class="nav_container container">
+    <div class="nav_inner row">
+        <div class="col-sm-3">
+            <div class="sidebar-nav">
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <span class="visible-xs navbar-brand">Sidebar menu</span>
+                    </div>
+                    <div class="nav_inner navbar-collapse collapse sidebar-navbar-collapse">
+                        <ul class="nav_list nav navbar-nav">
+                            <?php
+                            $who = 'user';
 
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul id="menu" class="nav navbar-nav">
-
-                <?php
-                    $who='user';
-                    
-                    $nav = new Navigation();
-                    $nav->loadXML("$who");
-                    
-
- 
-                ?>
-
-
-                
-                
-            </ul>
-<!--            <form class="navbar-form navbar-right" autocomplete="off">
-                     fake fields are a workaround for chrome autofill getting the wrong fields 
-                    <input style="display:none" type="text" name="fakeusernameremembered"/>
-                    <input style="display:none" type="password" name="fakepasswordremembered"/>
-                <div class="form-group">
-                    <input class="form-control" placeholder="Username" type="text" autocomplete="off">
+                            $nav = new Navigation();
+                            $nav->loadXML("$who");
+                            ?>
+                        </ul>
+                    </div><!--/.nav-collapse -->
                 </div>
-                <div class="form-group">
-                    <input class="form-control" placeholder="Password" type="password" autocomplete="off">
-                </div>
-                <button class="btn btn-success" type="submit">Sign in</button>
-            </form>-->
+            </div>
         </div>
-
+        <div class="nav_inner col-sm-9">
+            <p class="dibwars">DIBWARS</p>
+        </div>
     </div>
-</nav>
+</div>
+
+
+
