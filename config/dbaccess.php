1daf4f8403cb5277fd9a9341ab50234489ecce55
Pinkaro <schaller.alexander@hotmail.com>
2017-06-21 16:14:50 +0200

    goods overview, good movement ready

--- config/dbaccess.php
+++ config/dbaccess.php
@@ -48,4 +48,193 @@ class DB {
 //        $con->close();
 //        return $address;
 //    }
+    
+    function printGoodsList(){
+        $conn = $this->connect2DB();
+        
+        $stmt = "SELECT GoodsID, Name, StockAmount, CurrentNetSalesPrice FROM goods WHERE active = 1 ORDER BY GoodsID;";
+        
+        if($ergebnis = $conn->prepare($stmt)){
+            if($ergebnis->execute()){
+                $ergebnis->bind_result($id, $name, $stockAmount, $saleprice);
+                if($ergebnis){
+                    while($ergebnis->fetch()){
+                        echo "<tr class='goodCage'>";
+                        echo "<td class='good_id'>".$id."</td>";
+                        echo "<td>".$name."</td>";
+                        echo "<td>".$stockAmount."</td>";
+                        echo "<td>".$saleprice."</td>";
+                        echo "</tr>";
+                    }
+                }
+            }
+        }
+    }
+    
+    function getGoodById ($id) {       
+        $id = $id + 0;
+        $conn = $this->connect2DB();
+        $stmt = "SELECT g.GoodsID, 
+                        gc.Title, 
+                        g.Name, 
+                        g.Manufacturer, 
+                        g.CurrentNetSalesPrice AS Price,
+                        g.StorageLocation,
+                        g.Unit,
+                        g.MinAmount,
+                        g.StockAmount,
+                        s.Name AS supplier_name,
+                        t.Name AS tax,
+                        t.Percent 
+                    FROM goods g
+                    JOIN goodscategory gc USING (categoryid)
+                    JOIN supplier_has_goods USING (goodsid)
+                    JOIN taxes t USING (taxid)
+                    JOIN supplier s USING (supplierid)
+                    WHERE goodsid = ?";
+        if ($ergebnis = $conn->prepare($stmt)) {
+            $ergebnis->bind_param("i", $id);
+            if ($ergebnis->execute()) {
+                $ergebnis->bind_result( $goodsid, 
+                                        $category,
+                                        $name,
+                                        $manufacturer, 
+                                        $price, 
+                                        $storagelocation,
+                                        $unit,
+                                        $minamount,
+                                        $stockamount,
+                                        $supplier,
+                                        $taxname,
+                                        $taxpercent);
+                if ($ergebnis) {                   
+                    while ($ergebnis->fetch()) {
+                        $goodsarr = [];
+                        $goodsarr[0] = $goodsid;
+                        $goodsarr[1] = $category;
+                        $goodsarr[2] = $manufacturer;
+                        $goodsarr[3] = $price;
+                        $goodsarr[4] = $storagelocation;
+                        $goodsarr[5] = $unit;
+                        $goodsarr[6] = $minamount;
+                        $goodsarr[7] = $stockamount;
+                        $goodsarr[8] = $supplier;
+                        $goodsarr[9] = $taxname;
+                        $goodsarr[10] = $taxpercent;
+                        $goodsarr[11] = $name;
+                        
+                        return $goodsarr;
+                    }         
+                }  
+            }
+        }     
+    }
+    
+    //IS ACTUALLY INCOMING BUT TOO LAZY TO CORRECT
+    function printOutgoingMovementById($id, $asc){
+        $conn = $this->connection;
+        $stmt;
+        
+        if($asc){
+            $stmt = "SELECT 
+                        di.purchaseorderid, 
+                        phg.Amount AS OrderAmount, 
+                        dhg.Amount AS DeliveryAmount,
+                        po.DateTime AS OrderDate,
+                        di.IncomeDateTime AS DeliveryDate
+                    FROM purchaseorder_has_goods phg
+                    JOIN purchaseorder po USING (purchaseorderid)
+                    JOIN deliveryinfo di USING (purchaseorderid)
+                    JOIN delivery_has_goods dhg USING (deliveryinfoid)
+                    WHERE dhg.goodsid = ? AND po.status = 'completed'
+                    GROUP BY dhg.DeliveryInfoID, dhg.GoodsID
+                    ORDER BY deliverydate asc;";
+        }else{
+            $stmt = "SELECT 
+                        di.purchaseorderid, 
+                        phg.Amount AS OrderAmount, 
+                        dhg.Amount AS DeliveryAmount,
+                        po.DateTime AS OrderDate,
+                        di.IncomeDateTime AS DeliveryDate
+                    FROM purchaseorder_has_goods phg
+                    JOIN purchaseorder po USING (purchaseorderid)
+                    JOIN deliveryinfo di USING (purchaseorderid)
+                    JOIN delivery_has_goods dhg USING (deliveryinfoid)
+                    WHERE dhg.goodsid = ? AND po.status = 'completed'
+                    GROUP BY dhg.DeliveryInfoID, dhg.GoodsID
+                    ORDER BY deliverydate desc;";
+        }
+        
+        if ($ergebnis = $conn->prepare($stmt)) {
+            $ergebnis->bind_param("i", $id);
+            if ($ergebnis->execute()) {
+                $ergebnis->bind_result( $purchaseorderid, 
+                                        $orderamount,
+                                        $deliveryamount,
+                                        $orderdate,
+                                        $deliverydate);
+                if ($ergebnis) {                   
+                    while ($ergebnis->fetch()) {
+                        echo "<tr class='goods-entries'>";
+                        echo "<td data-field-type='number'>".$purchaseorderid."</td>";
+                        echo "<td data-field-type='number'>".$orderamount."</td>";
+                        echo "<td data-field-type='number'>".$deliveryamount."</td>";
+                        echo "<td data-field-type='date'>".$orderdate."</td>";
+                        echo "<td data-field-type='date' class='goods-inc-deliverydate'>".$deliverydate."</td>";
+                        echo "<tr>";
+                    }
+                }
+            }
+        }
+    }  
+    
+    //IS ACTUALLY OUTGOING, BUT TOO LAZY TO CORRECT
+    function printIncomingMovementById($id,$asc){
+        $conn = $this->connection;
+        $stmt;
+        
+        if($asc){
+            $stmt = "SELECT 
+                        so.salesorderid,
+                        shg.amount AS DeliveryAmount,
+                        so.DateTime AS OrderDate,
+                        so.OutgoingDateTime AS DeliveryDate
+                    FROM salesorder_has_goods shg
+                    JOIN salesorder so USING (salesorderid)
+                    WHERE shg.goodsid = ? AND so.status = 'completed'
+                    GROUP BY shg.goodsid, shg.salesorderid
+                    ORDER BY deliverydate asc;";
+        }else{
+            $stmt = "SELECT 
+                        so.salesorderid,
+                        shg.amount AS DeliveryAmount,
+                        so.DateTime AS OrderDate,
+                        so.OutgoingDateTime AS DeliveryDate
+                    FROM salesorder_has_goods shg
+                    JOIN salesorder so USING (salesorderid)
+                    WHERE shg.goodsid = ? AND so.status = 'completed'
+                    GROUP BY shg.goodsid, shg.salesorderid
+                    ORDER BY deliverydate desc;";
+        }
+        
+        if ($ergebnis = $conn->prepare($stmt)) {
+            $ergebnis->bind_param("i", $id);
+            if ($ergebnis->execute()) {
+                $ergebnis->bind_result( $salesorderid, 
+                                        $deliveryamount,
+                                        $orderdate,
+                                        $deliverydate);
+                if ($ergebnis) {                   
+                    while ($ergebnis->fetch()) {
+                        echo "<tr class='goods-entries'>";
+                        echo "<td data-field-type='number'>".$salesorderid."</td>";
+                        echo "<td data-field-type='number'>".$deliveryamount."</td>";
+                        echo "<td data-field-type='date'>".$orderdate."</td>";
+                        echo "<td data-field-type='date' class='goods-out-deliverydate'>".$deliverydate."</td>";
+                        echo "<tr>";
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ scripts/events.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    var incoming_goods_sort = "ascending";
+    var outgoing_goods_sort = "ascending";
+    
+
+    $(".goodCage").click(function () {
+        var id = $(this).find(".good_id").html();
+        window.location.href = '../FST2/ArticleMovement.php?product=' + id;
+    });
+
+    $(".goodCage").click(function () {
+        var id = $(this).find(".good_id").val();
+        $(this).attr('name');
+        window.location.href = '../FST2/ArticleMovement.php?product=' + id + "&";
+    });
+
+    $(".goods-incdate").click(function () {
+        var elems = $.makeArray($(this).parent().parent().parent().parent().find(".goods-entries"));
+        
+        if(incoming_goods_sort === "ascending"){
+            incoming_goods_sort = "descending";
+            $(".goods-incdate").wrapInner("");
+            $(".goods-incdate").wrapInner('<span title="Data sorted in ' + incoming_goods_sort + ' order"/>');
+            elems.sort(function (a, b) {
+                return parseDate($(a).find(".goods-inc-deliverydate").text()) < parseDate($(b).find(".goods-inc-deliverydate").text());
+            });
+            $(".movement-incoming-table").find(".movement-incoming-table-body").html(elems);
+        }else{
+            incoming_goods_sort = "ascending";
+            elems.sort(function (a, b) {
+                return parseDate($(a).find(".goods-inc-deliverydate").text()) > parseDate($(b).find(".goods-inc-deliverydate").text());
+            });
+            $(".movement-incoming-table").find(".movement-incoming-table-body").html(elems);
+        }
+            
+    });
+    
+    $(".goods-outdate").click(function () {
+        var elems = $.makeArray($(this).parent().parent().parent().parent().find(".goods-entries"));
+        
+        if(outgoing_goods_sort === "ascending"){
+            outgoing_goods_sort = "descending";
+
+            elems.sort(function (a, b) {
+                return parseDate($(a).find(".goods-out-deliverydate").text()) < parseDate($(b).find(".goods-out-deliverydate").text());
+            });
+            $(".movement-outgoing-table").find(".movement-outgoing-table-body").html(elems);
+        }else{
+            outgoing_goods_sort = "ascending";
+            elems.sort(function (a, b) {
+                return parseDate($(a).find(".goods-out-deliverydate").text()) > parseDate($(b).find(".goods-out-deliverydate").text());
+            });
+            $(".movement-outgoing-table").find(".movement-outgoing-table-body").html(elems);
+        }
+            
+    });
+});
+
+    function parseDate(input) {
+        var parts = input.match(/(\d+)/g);
+        // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+        return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]); //     months are 0-based
+    }
